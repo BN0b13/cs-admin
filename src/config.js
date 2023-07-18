ffcf956cc702e628f686195d9aefc4f98cff4217
BN0b13 <NobleNotes1@gmail.com>
2023-07-18 14:28:05 -0700

    updated config file to include dev

--- src/config.js
+++ src/config.js
@@ -4,6 +4,10 @@ export const tokenName = 'cosmicstrains-admin-token';
 export const api = 'https://api.cosmicstrains.com';
 export const url = 'https://admin.cosmicstrains.com';
 
+// Dev
+// export const api = 'https://api.dev.cosmicstrains.com';
+// export const url = 'https://admin.dev.cosmicstrains.com';
+
 // Local
 // export const api = 'http://localhost:8050';
 // export const url = 'http://localhost:5050';
