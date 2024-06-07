4e240f2d3d1c41024fd1efc9ab6f579a93949f50
BN0b13 <NobleNotes1@gmail.com>
2024-06-07 08:11:47 -0700

    changed config

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ export const tokenName = 'cosmicstrains-admin-token';
 export const themeTokenName = 'cs-admin-theme-token';
 
 // Prod
-// export const api = 'https://api.cosmicstrains.com';
-// export const url = 'https://admin.cosmicstrains.com';
+export const api = 'https://api.cosmicstrains.com';
+export const url = 'https://admin.cosmicstrains.com';
 
 // Dev
 // export const api = 'https://api.dev.cosmicstrains.com';
 // export const url = 'https://admin.dev.cosmicstrains.com';
 
 // Local
-export const api = 'http://localhost:8050';
-export const url = 'http://localhost:5050';
+// export const api = 'http://localhost:8050';
+// export const url = 'http://localhost:5050';
 
 
 export const productTypes = [
